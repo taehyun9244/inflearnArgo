6144b3ccc93d3a23bd2fa6e00cc00d9c5dcc834b
taehyun9244 <namtaehyun9244@gmail.com>
2022-04-19 22:31:14 +0900

    인프렁 알고리즘 8번문제

--- src/Argo_6.java
+++ src/Argo_6.java
@@ -8,7 +8,7 @@ public class Argo_6 {
         String str = br.readLine();
         String answer ="";
         for (int i = 0; i < str.length(); i++){
-            if (str.indexOf(str.charAt(i))== i){
+            if (str.indexOf(str.charAt(i))!= i){
                 answer += str.charAt(i);
             }
         }
--- src/Argo_8.java
+++ src/Argo_8.java
@@ -1,12 +1,29 @@
+//앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
+//
+//문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+//
+//단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
+//found7, time: study; Yduts; emit, 7Dnuof -> YES
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Argo_8 {
+    public String solution(String s){
+        String answer = "NO";
+        s = s.toLowerCase().replaceAll("[^a-z]", "");
+        System.out.println("s = " + s);
+        String tmp = new StringBuilder(s).reverse().toString();
+        if (tmp.equals(s)) answer = "YES";
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
+        Argo_8 Argo_8 = new Argo_8();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] strArr = new String[br.read()];
-
-
+        String str = br.readLine();
+        System.out.println(Argo_8.solution(str));
     }
 }
